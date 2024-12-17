3fb7fa7289a3950708bbd71b8686df1b9b6e655d
mrb0rman <keinihd@gmail.com>
2024-12-17 19:16:49 +0700

    fix

--- Assets/MirrorProjectTPU/Lab3/Script/CustomNetworkManager.cs
+++ Assets/MirrorProjectTPU/Lab3/Script/CustomNetworkManager.cs
@@ -50,7 +50,6 @@ namespace MirrorProjectTPU.Lab3.Script
         public override void OnStartServer()
         {
             StartCoroutine(ServerLoadSubScenes());
-            StopHost();
         }
 
         public override void OnStopClient()
